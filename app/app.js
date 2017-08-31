8ad5c629118b0d4b02ad2739be747c8aafe64005
Patrick Liesveld <pliesveld@users.noreply.github.com>
2017-08-31 08:15:45 -0400

    initial commit of issue template

--- app/app.js
+++ app/app.js
@@ -8,18 +8,43 @@ angular.module('App', ['ui.router'])
       .state('main',
         {
           url: '/',
-          templateUrl: 'partials/main.html'
+          templateUrl: 'partials/main.html', 
+          controller: function($scope, $stateParams) {
+            $scope.PARENT_ISSUE = "BOGUS-1234";
+            $scope.HOURS = 6;
+            $scope.TASK_ISSUE = "BOGUS-4321";
+            $scope.DESCRIPTION = "This is a sample description.  It is very descriptive";
+            $scope.IN = "PL";
+          }
         })
       .state('main.child1',
         {
           url: 'child1',
-          template: '<h3>child1</h3>'
-        })
+          templateUrl: 'partials/issue.html', 
+          controller: function($scope, $stateParams) {
+            $scope.TASK_ISSUE = "BOGUS-5432";
+          }
+ 
+    })
       .state('main.child2',
         {
           url: 'child2',
-          template: '<h3>child2</h3>'
+          templateUrl: 'partials/issue.html', 
+          controller: function($scope, $stateParams) {
+            $scope.DESCRIPTION = "Test something working";
+            $scope.TASK_ISSUE = "BOGUS-6543";
+          }
+        })
+      .state('main.child3',
+        {
+          url: 'child3',
+          templateUrl: 'partials/issue.html',
+          controller: function($scope, $stateParams) {
+            $scope.DESCRIPTION = "Test something broken";
+            $scope.TASK_ISSUE = "BOGUS-7654";
+          }
         });
+
 
 
   })
@@ -32,7 +57,6 @@ angular.module('App', ['ui.router'])
     });
   });
   
-
 $(document).ready(function() {
   console.log("document loaded");
 });
